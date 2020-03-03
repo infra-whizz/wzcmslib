939cf2583f6b4ab0e513b093ca3116b6a71b9e3c
Bo Maryniuk <bo@suse.de>
2020-03-03 17:48:44 +0100

    Add compiler cycle resolver

--- nanostate/compiler/reflist.go
+++ nanostate/compiler/reflist.go
@@ -13,14 +13,13 @@ type RefList struct {
 }
 
 func NewRefList() *RefList {
-	return new(RefList)
+	rl := new(RefList)
+	rl.Flush()
+	return rl
 }
 
 // Get all mentioned references
 func (rl *RefList) FindRefs(state *OTree) {
-	rl.included = make(map[string]bool)        // State IDs
-	rl.referenced_jobs = make(map[string]bool) // the entire blocks
-	rl.required_jobs = make(map[string]bool)   // their content
 	rl.findRefs(state)
 }
 
@@ -51,7 +50,7 @@ func (rl *RefList) GetIncluded() []string {
 // MarkVisited marks a reference as "seen" and "requested".
 // If it gets marked again, it means the request wasn't completed,
 // so we hit a infinite cycle, which needs to be broken out.
-func (rl *RefList) MarkVisited(id string) string {
+func (rl *RefList) MarkStateRequested(id string) string {
 	for _, mark := range rl.visited {
 		if mark == id {
 			panic(fmt.Errorf("State with ID '%s' still wasn't resolved", id))
@@ -61,6 +60,29 @@ func (rl *RefList) MarkVisited(id string) string {
 	return id
 }
 
+// MarkResolved marks a reference as "resolved" and removes from the stack
+func (rl *RefList) MarkStateResolved(id string) *RefList {
+	for idx, mark := range rl.visited {
+		if mark == id {
+			rl.visited = append(rl.visited[:idx], rl.visited[idx+1:]...)
+			break
+		}
+	}
+	delete(rl.included, id)
+
+	return rl
+}
+
+// Flush and forget everything
+func (rl *RefList) Flush() *RefList {
+	rl.included = make(map[string]bool)        // State IDs
+	rl.referenced_jobs = make(map[string]bool) // the entire blocks
+	rl.required_jobs = make(map[string]bool)   // their content
+	rl.visited = make([]string, 0)
+
+	return rl
+}
+
 func (rl *RefList) findRefs(state *OTree) {
 	for _, blockExpr := range state.GetBranch("state").Keys() {
 		expr := blockExpr.(string)
@@ -68,10 +90,10 @@ func (rl *RefList) findRefs(state *OTree) {
 			for _, expr_t := range strings.Split(expr, " ") {
 				if strings.HasPrefix(expr_t, "~") {
 					rl.included[strings.Split(expr_t, "/")[0][1:]] = true
-					rl.required_jobs[strings.Split(expr_t, "/")[1][1:]] = true
+					rl.required_jobs[strings.Split(expr_t, "/")[1]] = true
 				} else if strings.HasPrefix(expr_t, "&") {
 					rl.included[strings.Split(expr_t, "/")[0][1:]] = true
-					rl.referenced_jobs[strings.Split(expr_t, "/")[1][1:]] = true
+					rl.referenced_jobs[strings.Split(expr_t, "/")[1]] = true
 				}
 			}
 		}
