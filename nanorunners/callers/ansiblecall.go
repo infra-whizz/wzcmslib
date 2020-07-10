57eb574e2fa2162031e5400998b65431da1752ce
Bo Maryniuk <bo@maryniuk.net>
2020-07-10 17:03:13 +0200

    Bugfix: create config file after module type has been detected

--- nanorunners/callers/ansiblecall.go
+++ nanorunners/callers/ansiblecall.go
@@ -25,6 +25,7 @@ import (
 
 func NewAnsibleLocalModuleCaller(modulename string) *AnsibleModule {
 	am := new(AnsibleModule)
+	am.modType = 0
 	am.stateRoots = make([]string, 0)
 	am.name = strings.ToLower(strings.TrimPrefix(modulename, "ansible."))
 	am.args = map[string]interface{}{}
@@ -107,44 +108,52 @@ func (am *AnsibleModule) AddArg(key string, value interface{}) *AnsibleModule {
 // Call Ansible module
 func (am *AnsibleModule) Call() (map[string]interface{}, error) {
 	var ret map[string]interface{}
-	cfg, err := am.makeConfigFile()
+	stdout, stderr, err := am.execModule()
+	if stderr != "" {
+		fmt.Println(stderr)
+	}
+	if err != nil && stdout == "" && stderr == "" {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(stdout), &ret)
 	if err != nil {
 		return nil, err
-	} else {
-		defer os.Remove(cfg.Name())
-		stdout, stderr, err := am.execModule(cfg.Name())
-		if stderr != "" {
-			fmt.Println(stderr)
-		}
-		if err != nil && stdout == "" && stderr == "" {
-			return nil, err
-		}
-		err = json.Unmarshal([]byte(stdout), &ret)
-		if err != nil {
-			return nil, err
-		}
 	}
 
 	return ret, nil
 }
 
-func (am *AnsibleModule) execModule(cfgpath string) (string, string, error) {
+func (am *AnsibleModule) execModule() (string, string, error) {
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 
-	exePath, _ := am.resolveModulePath()
+	// Resolve module and set its type. Based on this, config file is made then
+	exePath, err := am.resolveModulePath()
+	if err != nil {
+		return "", "", err
+	}
+
+	// Create configuration JSON file
+	cfg, err := am.makeConfigFile()
+	if err != nil {
+		return "", "", err
+	}
+
+	defer os.Remove(cfg.Name())
+
 	var sh *exec.Cmd
 	if am.modType == BINARY {
-		sh = exec.Command(exePath, cfgpath)
+		sh = exec.Command(exePath, cfg.Name())
 	} else if am.modType == SCRIPT {
-		sh = exec.Command("python", exePath, cfgpath)
+		sh = exec.Command("python", exePath, cfg.Name())
 	} else {
 		return "", "", fmt.Errorf("Module %s was not found", am.name)
 	}
 	sh.Stdout = &stdout
 	sh.Stderr = &stderr
 
-	err := sh.Run()
+	err = sh.Run()
 	return stdout.String(), stderr.String(), err
 }
 
@@ -158,8 +167,10 @@ func (am *AnsibleModule) makeConfigFile() (*os.File, error) {
 	var data []byte
 	if am.modType == BINARY {
 		data, err = json.Marshal(am.args)
-	} else {
+	} else if am.modType == SCRIPT {
 		data, err = json.Marshal(map[string]interface{}{"ANSIBLE_MODULE_ARGS": am.args})
+	} else {
+		panic("An attempt to call an unresolved module type (binary or Ansible-native)")
 	}
 	if err != nil {
 		return nil, err
